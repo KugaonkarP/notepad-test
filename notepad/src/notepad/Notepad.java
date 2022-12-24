1abb42495036c3d54d5e3954f8f0379a4ac0dc2d
KugaonkarP <100780137+KugaonkarP@users.noreply.github.com>
2022-12-24 17:07:48 +0530

    Update Notepad.java

--- notepad/src/notepad/Notepad.java
+++ notepad/src/notepad/Notepad.java
@@ -1,130 +1,147 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.io.*;
+
+public class Simple_Text_Editor implements ActionListener {
+    JFrame frame;
+    JTextArea jTextArea;
+    JMenuBar jMenuBar;
+    JMenu File;
+    JMenu Edit;
+    JMenu Close;
+    JMenuItem NewFile;
+    JMenuItem OpenFile;
+    JMenuItem SaveFile;
+    JMenuItem PrintFile;
+    JMenuItem cut;
+    JMenuItem copy;
+    JMenuItem paste;
+    JMenuItem closeEditor;
+    Simple_Text_Editor(){
+        //creating the frame where everything gets displayed
+        frame = new JFrame("Notepad pro");
+        //setting up to which extent it should be visible
+        frame.setBounds(0,0,800,1000);
+        //Area where actual operation like writing text is done
+        jTextArea = new JTextArea("Welcome to the editor");
+        //creating menu bar where every overview of specific task there
+        jMenuBar = new JMenuBar();
+        File = new JMenu("File");
+        Edit = new JMenu("Edit");
+        Close = new JMenu("Close");
+
+        jMenuBar.add(File);
+        jMenuBar.add(Edit);
+        jMenuBar.add(Close);
 
 
-package notepad;
-    
-import java.awt.Font;
-import javax.swing.*;  
-import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import javax.swing.filechooser.FileNameExtensionFilter;
+        OpenFile = new JMenuItem("Open");
+        OpenFile.addActionListener(this);
+
+        SaveFile = new JMenuItem("Save");
+        SaveFile.addActionListener(this);
+
+        NewFile = new JMenuItem("New");
+        NewFile.addActionListener(this);
+
+        PrintFile = new JMenuItem("Print");
+        PrintFile.addActionListener(this);
+
+        //adding menu items to File menu
+        File.add(NewFile);
+        File.add(OpenFile);
+        File.add(SaveFile);
+        File.add(PrintFile);
 
 
-public class Notepad extends JFrame implements ActionListener{
-    JMenuBar menubar= new JMenuBar();
-    JMenu file=new JMenu("file");
-    JMenu edit=new JMenu("edit");
-    JMenu close=new JMenu("close");
-    JMenu help=new JMenu("help");
-    
-    JMenuItem newFile=new JMenuItem("new");
-    JMenuItem openFile=new JMenuItem("open");
-    JMenuItem saveFile=new JMenuItem("save");
-    JMenuItem closeFile=new JMenuItem("close");
-    
-    JTextArea textarea=new JTextArea();
-     Notepad(){
-    setTitle("First application");
-    setBounds(200,200,400,400);   
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    setLayout(null);
-    setJMenuBar(menubar);
-    menubar.add(file);
-    menubar.add(edit);
-    menubar.add(close);
-    menubar.add(help);
-    
-    file.add(newFile);
-    file.add(openFile);
-    file.add(closeFile);
-    file.add(saveFile);
-    
-    JScrollPane scroll=new JScrollPane(textarea);
-    add(scroll);
-    scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-    
-    textarea.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,16));
-    textarea.setLineWrap(true);
-    textarea.setWrapStyleWord(true);
-    
-    newFile.addActionListener(this);
-    openFile.addActionListener(this);
-    closeFile.addActionListener(this);
-    saveFile.addActionListener(this);
-    
+        copy = new JMenuItem("Copy");
+        copy.addActionListener(this);
+
+        cut = new JMenuItem("Cut");
+        cut.addActionListener(this);
+
+        paste = new JMenuItem("Paste");
+        paste.addActionListener(this);
+
+        //adding menu items to Edit menu
+        Edit.add(copy);
+        Edit.add(cut);
+        Edit.add(paste);
+
+
+        closeEditor = new JMenuItem("Close");
+        closeEditor.addActionListener(this);
+
+        Close.add(closeEditor);
+
+
+        frame.setJMenuBar(jMenuBar);
+        frame.add(jTextArea);
+        //program will stop after closing the editor
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
-    
-    /**
-     * @param args the command line arguments
-     */
-    
+    public static void main(String[] args) {
+        Simple_Text_Editor editor = new Simple_Text_Editor();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equalsIgnoreCase("New")) {
-            textarea.setText(null);
-        }else if (e.getActionCommand().equalsIgnoreCase("Open")) {
-            JFileChooser fileChooser=new JFileChooser();
-            FileNameExtensionFilter textFilter=new FileNameExtensionFilter("only txt file", "txt");
-            fileChooser.setAcceptAllFileFilterUsed(false);
-            fileChooser.addChoosableFileFilter(textFilter);
-            
-            int action=fileChooser.showOpenDialog(null);
-            if (action!=JFileChooser.APPROVE_OPTION) {
-                return;
-            }else{
-               try{
-                    BufferedReader reader=new BufferedReader(new FileReader(fileChooser.getSelectedFile()));
-                    textarea.read(reader,null);
-                }catch(IOException ex){
-                    ex.printStackTrace();
-                } 
+        String s = e.getActionCommand();
+        if(s.equals("Copy")){
+            jTextArea.copy();
+        }else if(s.equals("Cut")){
+            jTextArea.cut();
+        }else if(s.equals("Paste")){
+            jTextArea.paste();
+        }else if(s.equals("Print")){
+            try {
+                jTextArea.print();
+            } catch (PrinterException ex) {
+                throw new RuntimeException(ex);
             }
-            
-            
-        }else if (e.getActionCommand().equalsIgnoreCase("Save")) {
-            JFileChooser fileChooser=new JFileChooser();
-            FileNameExtensionFilter textFilter=new FileNameExtensionFilter("only txt file", "txt");
-            fileChooser.setAcceptAllFileFilterUsed(false);
-            fileChooser.addChoosableFileFilter(textFilter);
-            
-            int action=fileChooser.showSaveDialog(null);
-            
-            if (action!=JFileChooser.APPROVE_OPTION) {
-                return; 
-            }else{
-                String filename=fileChooser.getSelectedFile().getAbsolutePath().toString();
-                if (!filename.contains(".txt")) {
-                    filename=filename+".txt";
-                     
-                try{
-                    BufferedWriter writer=new BufferedWriter(new FileWriter(filename));
-                    textarea.write(writer);
-                }catch(IOException ex){
-                    ex.printStackTrace();
-                }
+        }else if(s.equals("New")){
+            jTextArea.setText("");
+        }else if(s.equals("Close")){
+            frame.setVisible(false);
+        }else if(s.equals("Open")){
+            JFileChooser jFileChooser = new JFileChooser("C:");
+
+            int ans = jFileChooser.showOpenDialog(null);
+            if(ans == JFileChooser.APPROVE_OPTION){
+                File file = new File(jFileChooser.getSelectedFile().getAbsolutePath());
+                String s1="",s2 = "";
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                    s2 = bufferedReader.readLine();
+                    while((s1= bufferedReader.readLine())!=null){
+                        s2 += s1+"\n";
+                    }
+                    jTextArea.setText(s2);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
                 }
             }
-            
-            
-        }else if (e.getActionCommand().equalsIgnoreCase("close")) {
-            
+        }else if(s.equals("Save")){
+            JFileChooser jFileChooser = new JFileChooser("C:");
+            int ans = jFileChooser.showOpenDialog(null);
+            if(ans==JFileChooser.APPROVE_OPTION){
+                File file = new File(jFileChooser.getSelectedFile().getAbsolutePath());
+                BufferedWriter writer = null;
+                try {
+                    writer = new BufferedWriter(new FileWriter(file,false));
+                    writer.write((jTextArea.getText()));
+                    writer.flush();
+                    writer.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
         }
-    
     }
-    
-    public static void main(String[] args) throws Exception{
-        // TODO code application logic here
-       
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        
-        new Notepad().setVisible(true);
-        
-    }
-    
 }
